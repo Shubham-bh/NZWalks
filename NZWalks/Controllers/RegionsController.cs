4ebb43bc21fee4d22504134bb40e5410342b8558
LENOVO <LENOVO@DESKTOP-VTKB3MN>
2025-08-05 23:52:01 +0530

    Added Regions Controller

---
+++ NZWalks/Controllers/RegionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Data;
+using NZWalks.Models.Domain;
+
+namespace NZWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+        }
+
+        //GET SINGLE REGION BY ID
+        //api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetRegionById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+
+    }
+}
